2a5009d62a0cf283e12fabb885104365cbe0a825
jorge-marroquin <jordy.orantes@meca.com.gt>
2026-04-08 10:45:27 -0600

    cambuo

---
+++ l10n_gt_settings_git/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Localizacion Guatemalteca",
+    'summary': """Módulo que añade el menú Guatemala al menú Localizaciones de la aplicación de Ajustes, versión 18""",
+    'description': """1. Añade el menú Guatemala al menú Localizaciones de la aplicación de Ajustes""",
+    'author': "",
+    'support': '',
+    'website': "",
+    'category': 'Technical Settings',
+    'version': '0.1',
+    'license': "AGPL-3",
+    'depends': ['base'],
+    'data': ['views/res_config_settings.xml'],
+    'application': True,
+    'sequence': 1,
+}
